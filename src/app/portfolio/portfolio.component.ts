8c5fb6cd530b0a188a9d8c425bb531c7d9174f74
aymen065 <mohamedhouidi615@gmail.com>
2024-09-18 08:52:49 +0100

    commit 7

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,21 +12,21 @@ export class PortfolioComponent implements OnInit {
   constructor(public dialog: MatDialog) { }
 
   projects = [
+    
     {
       id: 0,
-      name: 'E-Store',
-      images: ['assets/img/work/ecomm/1.png', 'assets/img/work/ecomm/2.png', 'assets/img/work/ecomm/3.png', 'assets/img/work/ecomm/4.png', 'assets/img/work/ecomm/5.png', 'assets/img/work/ecomm/6.png', 'assets/img/work/ecomm/7.png', 'assets/img/work/ecomm/8.png'],
-      technologies:["Spring Boot 2.7","Angular 12","MySQL 5.7"],
-      date: "De 1 févr. 2022 À 23 févr. 2022",
-      description: "This application is intended for clients, administrators, influencers, and advertisers of various brands and shops, to allow them to interact with each other in an automatically organized manner."
-    },
-    {
-      id: 1,
       name: 'LocationTN',
       images: ['assets/img/work/loc/1.PNG', 'assets/img/work/loc/2.PNG', 'assets/img/work/loc/3.PNG','assets/img/work/loc/4.PNG','assets/img/work/loc/5.PNG','assets/img/work/loc/6.PNG','assets/img/work/loc/7.PNG','assets/img/work/loc/8.PNG','assets/img/work/loc/9.PNG','assets/img/work/loc/10.PNG'],
       technologies:["Spring Boot 2.7","Thymeleaf","MySQL 5.7"],
       date:"",
       description: "This project involves developing a web application named 'locationTN.' It will be aimed at people looking to rent a car without wasting time, providing all the necessary information to ensure customer satisfaction. On the other hand, this application is also intended for the company's managers to facilitate interaction with customers."
+    },{
+      id: 1,
+      name: 'E-Store',
+      images: ['assets/img/work/ecomm/1.png', 'assets/img/work/ecomm/2.png', 'assets/img/work/ecomm/3.png', 'assets/img/work/ecomm/4.png', 'assets/img/work/ecomm/5.png', 'assets/img/work/ecomm/6.png', 'assets/img/work/ecomm/7.png', 'assets/img/work/ecomm/8.png'],
+      technologies:["Spring Boot 2.7","Angular 12","MySQL 5.7"],
+      date: "De 1 févr. 2022 À 23 févr. 2022",
+      description: "This application is intended for clients, administrators, influencers, and advertisers of various brands and shops, to allow them to interact with each other in an automatically organized manner."
     },
     
   ];
